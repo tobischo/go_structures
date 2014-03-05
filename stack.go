96dd557905f373508a3a770cdae3818b216d6985
Tobias Schoknecht <tobias.schoknecht@gmail.com>
2014-03-05 21:42:49 +0100

    Added stack

---
+++ stack.go
@@ -0,0 +1,41 @@
+package go_structures
+
+import (
+	"errors"
+)
+
+// Stack which is exposed to be used by other packages
+type Stack struct {
+	top  *element
+	size int
+}
+
+// Unexposed helper structure to allow the stack to grow
+type element struct {
+	value interface{}
+	next  *element
+}
+
+// Adds any value to the stack
+func (stack *Stack) Push(value interface{}) {
+	stack.top = &element{value, stack.top}
+	stack.size++
+}
+
+// Removes the top element from the stack and returns it
+// Returns an error if the stack is empty
+func (stack *Stack) Pop() (interface{}, error) {
+	if stack.size == 0 {
+		return nil, errors.New("Stack empty")
+	}
+
+	value := stack.top.value
+	stack.top = stack.top.next
+	stack.size--
+	return value, nil
+}
+
+// Returns the current size of the stack
+func (stack *Stack) Size() int {
+	return stack.size
+}
